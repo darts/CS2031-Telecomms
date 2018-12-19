6c4c0887c1bc9495011c7d5d5917c083efc72dbf
dartse <darts@tcd.ie>
2018-12-19 14:58:05 +0000

    Automate the Updating of Environment
    No more manual copying.

---
+++ Launch/a2/copyFiles.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+cp ~/eclipse-workspace/TelecommsAssignment2/src/* //cs2031/a2/controller
+javac //cs2031/a2/controller/*.java
+cp //cs2031/a2/controller/* //cs2031/a2/r1
+cp //cs2031/a2/controller/* //cs2031/a2/r2
+cp //cs2031/a2/controller/* //cs2031/a2/e1
+cp //cs2031/a2/controller/* //cs2031/a2/e2
